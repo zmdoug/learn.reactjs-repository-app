151d7d9c1eced69bb820bd15c5ee494c82c2b3e6
Douglas Maehler Zardinello <zmdoug@gmail.com>
2020-01-11 01:17:10 -0300

    Service structure, button animation

--- app/src/styles/global.js
+++ app/src/styles/global.js
@@ -16,4 +16,14 @@ body {
   background: #7159c1;
   -webkit-font-smoothing: antialiased !important;
 }
+
+body, input, button {
+  color:#222;
+  font-size: 14px;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+button {
+  cursor: pointer;
+}
 `;
